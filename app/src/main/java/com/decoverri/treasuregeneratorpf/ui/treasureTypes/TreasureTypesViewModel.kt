ab1c62d047a69aeddb92ae021ef03b316cca4381
Deco Verri <andre.verri@infinityworks.com>
2023-01-18 14:29:18 +0000

    delet unused viewmodel

--- app/src/main/java/com/decoverri/treasuregeneratorpf/ui/treasureTypes/TreasureTypesViewModel.kt
+++
@@ -1,9 +0,0 @@
-package com.decoverri.treasuregeneratorpf.ui.treasureTypes
-
-import androidx.lifecycle.ViewModel
-import com.decoverri.treasuregeneratorpf.model.TreasureType
-
-class TreasureTypesViewModel : ViewModel() {
-    val types = TreasureType.values()
-
-}
